ed771132ce8b8f3cc2631a610e8663c92a0c1668
Ibrahim <luvettealukwe@gmail.com>
2023-07-03 06:16:33 +0300

    update

--- Assignment2.py
+++ Assignment2.py
@@ -7,7 +7,7 @@ elif marks >= 80 and marks < 90:
     print ("Grade A+")
 elif marks >= 70 and marks < 80:
     print ("Grade A")
-elif marks > =60 and marks < 70:
+elif marks >= 60 and marks < 70:
     print ("Grade B+")
 elif marks >= 50 and marks < 60:
     print ("Grade B")
